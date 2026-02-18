23f339afb56be4886c9a1ca4a5015b69e99e45ac
Camila Rapu Honores <kazy152001@gmail.com>
2026-02-18 11:36:24 -0300

    File dialog from resource picker updated

--- addons/sprouty_dialogs/editor/components/scripts/resource_picker.gd
+++ addons/sprouty_dialogs/editor/components/scripts/resource_picker.gd
@@ -15,17 +15,21 @@ signal resource_picked(res: Resource)
 signal clear_pressed
 
 ## Resource types enum
-enum ResourceType {DIALOG_CHAR, DIALOGUE, CHARACTER, SCENE}
+enum ResourceType {DIALOG_CHAR, DIALOGUE, CHARACTER, PORTRAIT_SCENE, DIALOG_BOX}
 
-## Resource type to search for
+## Resource type to load
 @export var resource_type: ResourceType = ResourceType.DIALOG_CHAR
 ## If true, show the icon button without the arrow button
 @export var only_icon: bool = false
 ## If true, a clear button will be added to the popup menu
 @export var add_clear_button: bool = false
 
-## The options popup menu
-var popup_menu: Object
+## File filters for load dialog
+var _file_filters: PackedStringArray
+## File type to load the last used path
+var _recent_file_type: String
+## File dialog title
+var _dialog_title: String
 
 
 func _ready() -> void:
@@ -35,35 +39,67 @@ func _ready() -> void:
 	if only_icon: # Show the icon button without the arrow button
 		get_child(2).icon = get_theme_icon("Load", "EditorIcons")
 		remove_child(get_child(1))
+	
+	set_resource_type(resource_type)
 
-	match resource_type:
+
+## Set the resource type to load
+func set_resource_type(type: ResourceType) -> void:
+	resource_type = type
+	match type:
 		ResourceType.DIALOG_CHAR:
 			base_type = "SproutyDialogsDialogueData,SproutyDialogsCharacterData"
+			_dialog_title = "Open a File"
+			_recent_file_type = "sprouty_files"
+			_file_filters = ["*.tres"]
 		ResourceType.DIALOGUE:
 			base_type = "SproutyDialogsDialogueData"
+			_dialog_title = "Open a Dialogue"
+			_recent_file_type = "dialogue_files"
+			_file_filters = ["*.tres"]
 		ResourceType.CHARACTER:
 			base_type = "SproutyDialogsCharacterData"
-		ResourceType.SCENE:
+			_dialog_title = "Open a Character"
+			_recent_file_type = "character_files"
+			_file_filters = ["*.tres"]
+		ResourceType.PORTRAIT_SCENE:
 			base_type = "PackedScene"
+			_dialog_title = "Open a Portrait Scene"
+			_recent_file_type = "portrait_files"
+			_file_filters = ["*.tscn"]
+		ResourceType.DIALOG_BOX:
+			base_type = "PackedScene"
+			_dialog_title = "Open a Dialog Box Scene"
+			_recent_file_type = "dialog_box_files"
+			_file_filters = ["*.tscn"]
 
 
 func _set_create_options(menu_node: Object) -> void:
+	if not menu_node.is_connected("id_pressed", _on_popup_id_pressed):
+		menu_node.id_pressed.connect(_on_popup_id_pressed)
 	if add_clear_button:
-		menu_node.add_icon_item(get_theme_icon("Clear", "EditorIcons"), "Clear", 3)
+		menu_node.add_icon_item(get_theme_icon("Clear", "EditorIcons"), "Clear", 2)
 		menu_node.add_separator()
-		if not menu_node.is_connected("id_pressed", _on_popup_id_pressed):
-			menu_node.id_pressed.connect(_on_popup_id_pressed)
-		popup_menu = menu_node
 	pass # Avoid new resource options
 
 
+## Handle when an item from the pop-up menu is pressed
 func _on_popup_id_pressed(id: int) -> void:
 	match id:
-		3: # Clear button selected
+		0: # Load button pressed
+			var file_dialog = get_child(get_child_count() - 1)
+			file_dialog.set_current_dir(
+				SproutyDialogsFileUtils.get_recent_file_path(_recent_file_type))
+			file_dialog.filters = _file_filters
+			file_dialog.title = _dialog_title
+		1: # Quick load button pressed
+			pass
+		2: # Clear button pressed
 			clear_pressed.emit()
 
 
 ## Handle when resources are changed
 func _on_resource_changed(res: Resource) -> void:
+	SproutyDialogsFileUtils.set_recent_file_path(_recent_file_type, res.resource_path)
 	edited_resource = null
 	resource_picked.emit(res)
--- addons/sprouty_dialogs/editor/modules/file_manager/scripts/file_manager.gd
+++ addons/sprouty_dialogs/editor/modules/file_manager/scripts/file_manager.gd
@@ -23,8 +23,6 @@ signal all_character_files_closed
 @onready var _new_dialog_button: Button = %NewDialogButton
 ## New character button
 @onready var _new_char_button: Button = %NewCharButton
-## Open file button
-@onready var _open_file_button: Button = %OpenFileButton
 ## Save file button
 @onready var _save_file_button: Button = %SaveFileButton
 
@@ -58,7 +56,6 @@ func _ready() -> void:
 	# Connect signals
 	_new_dialog_button.pressed.connect(on_new_dialog_pressed)
 	_new_char_button.pressed.connect(on_new_character_pressed)
-	_open_file_button.pressed.connect(on_open_file_pressed)
 	_save_file_button.pressed.connect(save_file)
 
 	open_file_dialog.file_selected.connect(load_file)
@@ -74,7 +71,6 @@ func _ready() -> void:
 	_csv_file_field.path_changed.connect(_on_csv_file_path_changed)
 
 	# Set icons for buttons
-	_open_file_button.icon = get_theme_icon("Folder", "EditorIcons")
 	_save_file_button.icon = get_theme_icon("Save", "EditorIcons")
 	_new_dialog_button.icon = _new_dialog_icon
 	_new_char_button.icon = _new_char_icon
@@ -198,7 +194,7 @@ func load_file(path: String) -> void:
 	
 	if FileAccess.file_exists(path):
 		var resource = load(path)
-		SproutyDialogsFileUtils.set_recent_file_path("graph_dialogs_files", path)
+		SproutyDialogsFileUtils.set_recent_file_path("sprouty_files", path)
 
 		if resource is SproutyDialogsDialogueData:
 			SproutyDialogsFileUtils.set_recent_file_path("dialogue_files", path)
@@ -282,7 +278,7 @@ func save_file(index: int = _file_list.get_current_index(), path: String = "") -
 
 ## Open file dialog to select a file
 func on_open_file_pressed() -> void:
-	open_file_dialog.set_current_dir(SproutyDialogsFileUtils.get_recent_file_path("graph_dialogs_files"))
+	open_file_dialog.set_current_dir(SproutyDialogsFileUtils.get_recent_file_path("sprouty_files"))
 	open_file_dialog.popup_centered()
 
 
