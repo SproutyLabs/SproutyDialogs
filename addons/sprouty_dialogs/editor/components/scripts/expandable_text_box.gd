f1c74ef972601917c610ad46044824c097be907b
Camila Rapu Honores <kazy152001@gmail.com>
2026-03-24 16:43:49 -0300

    Update text box size

--- addons/sprouty_dialogs/editor/components/scripts/expandable_text_box.gd
+++ addons/sprouty_dialogs/editor/components/scripts/expandable_text_box.gd
@@ -18,11 +18,11 @@ signal update_text_editor(text_box: TextEdit)
 ## Emitted when the text box loses focus
 signal text_box_focus_exited()
 
+const MAX_VISIBLE_LINES := 3
+const EXTRA_Y_PADDING := 8
+
 ## Input text box
 @onready var _text_box: TextEdit = $TextEdit
-
-const MAX_VISIBLE_LINES := 4
-const EXTRA_Y_PADDING := 8
 
 
 func _ready():
@@ -31,15 +31,6 @@ func _ready():
 	_text_box.focus_entered.connect(update_text_editor.emit.bind(_text_box))
 	$ExpandButton.pressed.connect(open_text_editor.emit.bind(_text_box))
 	$ExpandButton.icon = get_theme_icon("DistractionFree", "EditorIcons")
-	_on_text_changed_internal()
-
-
-func _on_text_changed_internal(_arg: Variant = null) -> void:
-	text_changed.emit(_text_box.text)
-	var lines := mini(_text_box.get_total_visible_line_count(), MAX_VISIBLE_LINES)
-	var h := lines * _text_box.get_line_height() + EXTRA_Y_PADDING
-	_text_box.custom_minimum_size.y = h
-	custom_minimum_size.y = h
 
 
 ## Returns the text from the text box
@@ -50,3 +41,18 @@ func get_text() -> String:
 ## Set the text to the text box
 func set_text(text: String) -> void:
 	_text_box.text = text
+	_update_text_box_size()
+
+
+## Update the size of the text box by the line count
+func _update_text_box_size() -> void:
+	var lines := mini(_text_box.get_total_visible_line_count(), MAX_VISIBLE_LINES)
+	var h := lines * _text_box.get_line_height() + EXTRA_Y_PADDING
+	_text_box.custom_minimum_size.y = h
+	custom_minimum_size.y = h
+
+
+## Handle the text changed signal
+func _on_text_changed_internal(_arg: Variant = null) -> void:
+	text_changed.emit(_text_box.text)
+	_update_text_box_size()
